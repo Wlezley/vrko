ccd68c04ab484c265d040a0afcc15318259cd2fa
Wlezley Lishack <wlezley.lishack@gmail.com>
2021-10-18 23:56:47 +0200

    Oprava formulare pro autorizaci rezervace

--- app/Forms/ReservationForms/ReservationAuthorizeFormFactory.php
+++ app/Forms/ReservationForms/ReservationAuthorizeFormFactory.php
@@ -48,36 +48,22 @@ class ReservationAuthorizeFormFactory extends Control
 
 		$form->addProtection('Vypršel časový limit, odešlete formulář znovu');
 
-		//$email = (empty($this->exchanger['email']) ? NULL : $this->exchanger['email']);
-		//$authCode = (empty($this->exchanger['authCode']) ? NULL : $this->exchanger['authCode']);
-
 		$form->addText('email',								'Email')
 			->setHtmlAttribute('placeholder',				'Email')
 			->addRule(Form::EMAIL, 'Zadejte platný Email.')
 			->addRule(Form::MAX_LENGTH, 'EMAIL: Maximálně 64 znaků.', 64)
-			->setRequired('Zadejte platný Email.');
-			//->setValue($email);
+			->setRequired('Zadejte platný Email.')
+			->setDisabled(!empty($this->exchanger['email']))
+			->setValue(empty($this->exchanger['email']) ? "" : $this->exchanger['email'])
+			->setHtmlAttribute('autofocus', empty($this->exchanger['email']));
 
 		$form->addText('authCode',							'SMS Kód')
 			->setHtmlAttribute('placeholder',				'SMS Kód')
 			->setHtmlType('tel')
 			->addRule(Form::LENGTH, 'Zadejte celý kód.', 4)
-			->setRequired('Zadejte SMS kód.');
-			//->setValue($authCode);
-
-		if(!empty($this->exchanger['email'])) {
-			$form['email']->setDisabled();
-			$form['email']->setValue($this->exchanger['email']);
-			$form['authCode']->setHtmlAttribute('autofocus', true);
-		}
-		else {
-			$form['email']->setHtmlAttribute('autofocus', true);
-		}
-
-		/*if(!empty($this->exchanger['authCode']))
-		{
-			$form['authCode']->setValue($this->exchanger['authCode']);
-		}*/
+			->setRequired('Zadejte SMS kód.')
+		//	->setValue(empty($this->exchanger['authCode']) ? "" : $this->exchanger['authCode'])
+			->setHtmlAttribute('autofocus', !empty($this->exchanger['email']));
 
 		$form->addSubmit('send', 'Odeslat');
 
